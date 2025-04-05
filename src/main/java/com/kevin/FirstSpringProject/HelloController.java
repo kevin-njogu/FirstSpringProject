d75dd2d123a2722b0097d6a1ca68de78558770e9
Kevin <kelvnjogu@gmail.com>
2025-04-05 20:42:56 +0300

    Initial commit

---
+++ src/main/java/com/kevin/FirstSpringProject/HelloController.java
@@ -0,0 +1,22 @@
+package com.kevin.FirstSpringProject;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public HelloResponse hello() {
+        return new HelloResponse("Hello World!!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse hello(@PathVariable String name) {
+        return new HelloResponse("Hello World!! + " + name);
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        return new HelloResponse("Hello " + name + " !");
+    }
+}
